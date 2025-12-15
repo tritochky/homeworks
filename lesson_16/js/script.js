cd153b370de7b8a5d39498e347838d2530dc2bf8
Olga Fomenko <76599740+tritochky@users.noreply.github.com>
2025-12-15 15:32:38 +0200

    Add files via upload

--- lesson_16/js/script.js
+++ lesson_16/js/script.js
@@ -4,7 +4,7 @@ document.addEventListener('click', documentActions)
 function documentActions(e) {
     const targetElement = e.target
 	
-    if (targetElement.closest('.menu__icon')) {
+    if (targetElement.closest('.header__menu-icon')) {
         document.documentElement.toggleAttribute('data-menu-open')
     }
 }
